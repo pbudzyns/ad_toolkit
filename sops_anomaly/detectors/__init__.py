ba4bd4d839dee83da758adad0394af00f70506c4
pbudzyns <pawel.budzynski19+git@gmail.com>
2022-07-28 10:04:22 +0200

    condidtional import of donut

--- sops_anomaly/detectors/__init__.py
+++ sops_anomaly/detectors/__init__.py
@@ -1,7 +1,6 @@
 from .autoencoder import AutoEncoder
 from .ae_tss import AutoEncoderTSS
 from .base_detector import BaseDetector
-from .donut_ad import Donut
 from .lstm_ad import LSTM_AD
 from .lstm_ed import LSTM_ED
 from .random_ad import RandomDetector
@@ -11,9 +10,14 @@ __all__ = [
     'AutoEncoder',
     'AutoEncoderTSS',
     'BaseDetector',
-    'Donut',
     'LSTM_AD',
     'LSTM_ED',
     'RandomDetector',
     'VariationalAutoEncoder',
 ]
+
+try:
+    from .donut_ad import Donut
+    __all__ += ['Donut']
+except ImportError:
+    pass
