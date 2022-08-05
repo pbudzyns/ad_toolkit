68778ea63eb87e435e1f506862ed527ed80cbfe7
pbudzyns <pawel.budzynski19+git@gmail.com>
2022-08-05 12:55:05 +0200

    add missing ae_tss test

--- tests/test_detectors/test_ae_tss.py
+++ tests/test_detectors/test_ae_tss.py
@@ -49,6 +49,30 @@ def test_train_predict_ae_tss(data, window_size, latent_size):
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason='no cuda device')
 @pytest.mark.parametrize("data", datasets)
+@pytest.mark.parametrize("window_size", (1, 3, 5))
+@pytest.mark.parametrize("latent_size", (5, 10))
+def test_train_predict_ae_tss_gpu(data, window_size, latent_size):
+    ae = AutoEncoderTSS(
+        window_size=window_size, latent_size=latent_size, use_gpu=True)
+    ae.train(data, epochs=2)
+
+    p = ae.predict(data)
+    assert len(p) == len(data)
+
+
+@pytest.mark.parametrize("data", datasets)
+@pytest.mark.parametrize("window_size", (3, 5))
+def test_train_detect_ae_tss(data, window_size):
+    ae = AutoEncoderTSS(window_size=window_size, latent_size=10)
+    ae.train(data, epochs=2)
+
+    p = ae.detect(data)
+    assert len(p) == len(data)
+    assert all(pp in (0, 1) for pp in p)
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason='no cuda device')
+@pytest.mark.parametrize("data", datasets)
 @pytest.mark.parametrize("window_size", (3, 5))
 def test_train_detect_ae_tss_gpu(data, window_size):
     ae = AutoEncoderTSS(window_size=window_size, latent_size=10, use_gpu=True)
